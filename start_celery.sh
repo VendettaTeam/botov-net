cb0eb243b95f7b2c65d60d4cd3d214e80eda12c2
Valery Krasnoselsky <valery.krasnoselsky@gmail.com>
2019-11-05 22:06:04 +0300

    debug mode for celery

--- start_celery.sh
+++ start_celery.sh
@@ -2,9 +2,12 @@
 
 export $(cat .env | xargs)
 
-if [ $(uname -s) == "MINGW64_NT-6.1" ]
-then
+if [ $(uname -s) == "MINGW64_NT-6.1" ]; then
   celery -A project.settings worker -l into -P eventlet
 else
-  celery worker -A project.settings
+  if [[ $DJANGO_PROJECT_DEBUG == "True" ]]; then
+    celery worker -A project.settings
+  else
+    celery worker -A project.settings -l ERROR
+  fi
 fi
